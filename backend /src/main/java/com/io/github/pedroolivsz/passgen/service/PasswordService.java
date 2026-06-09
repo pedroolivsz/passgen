0a33baddbf7f16cfb3bac77a19256cff052384aa
Pedro <joaopedropereira2209@gmail.com>
2026-06-09 10:52:27 -0300

    feat (PasswordService): Created function buildAlphabet

--- backend /src/main/java/com/io/github/pedroolivsz/passgen/service/PasswordService.java
+++ backend /src/main/java/com/io/github/pedroolivsz/passgen/service/PasswordService.java
@@ -1,8 +1,27 @@
 package com.io.github.pedroolivsz.passgen.service;
 
+import com.io.github.pedroolivsz.passgen.DTOs.PasswordRequest;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
 
 @Service
 public class PasswordService {
+    private static final SecureRandom RANDOM = new SecureRandom();
 
+    private String buildAlphabet(PasswordRequest request) {
+        StringBuilder alphabet = new StringBuilder();
+
+        if(request.uppercase()) alphabet.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        if(request.lowercase()) alphabet.append("abcdefghijklmnopqrstuvwxyz");
+        if(request.numbers()) alphabet.append("0123456789");
+        if(request.symbols()) alphabet.append("!@#$%^&*()_+-=[]{}");
+
+        String pool = alphabet.toString();
+
+        if(request.excludeAmbiguous()) {
+            pool = pool.replaceAll("[l1IO0oB8S5Z2]", "");
+        }
+        return pool;
+    }
 }
