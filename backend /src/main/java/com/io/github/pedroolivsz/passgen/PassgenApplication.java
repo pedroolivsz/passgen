eaf98bb3499e1b6bd7a334a1560db457c7e525d2
Pedro <joaopedropereira2209@gmail.com>
2026-06-09 18:39:10 -0300

    feat: Added main

---
+++ backend /src/main/java/com/io/github/pedroolivsz/passgen/PassgenApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.pedroolivsz.passgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassgenApplication.class, args);
+	}
+
+}
