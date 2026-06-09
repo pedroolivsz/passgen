73134b18961604a2d273ed2bb7a4d46258eb69f1
Pedro <joaopedropereira2209@gmail.com>
2026-06-09 18:32:56 -0300

    feat: Created PasswordController

---
+++ backend /src/main/java/com/io/github/pedroolivsz/passgen/controller/PasswordController.java
@@ -0,0 +1,10 @@
+package com.io.github.pedroolivsz.passgen.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/passwords")
+public class PasswordController {
+
+}
