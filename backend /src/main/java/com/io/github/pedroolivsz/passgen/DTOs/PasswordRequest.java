0cefb4ee88d67f8741adff0368522e1211c117a3
Pedro <joaopedropereira2209@gmail.com>
2026-06-08 22:47:03 -0300

    feat: Addition of request parameters

--- backend /src/main/java/com/io/github/pedroolivsz/passgen/DTOs/PasswordRequest.java
+++ backend /src/main/java/com/io/github/pedroolivsz/passgen/DTOs/PasswordRequest.java
@@ -1,5 +1,22 @@
 package com.io.github.pedroolivsz.passgen.DTOs;
 
-public record PasswordRequest() {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
+public record PasswordRequest(
+        @Min(value = 8)
+        @Max(value = 128)
+        Integer length,
+        boolean uppercase,
+        boolean lowercase,
+        boolean numbers,
+        boolean symbols,
+        @Min(1)
+        @Max(20)
+        Integer quantity,
+        boolean excludeAmbiguous
+) {
+    public PasswordRequest() {
+        this(16, true, true, true, false, 1, false);
+    }
 }
