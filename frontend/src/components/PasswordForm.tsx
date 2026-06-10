d96509cd3fd5e99756e6a49f6983df59c4f56287
Pedro <joaopedropereira2209@gmail.com>
2026-06-10 08:51:31 -0300

    feat: Add atributes

--- frontend/src/components/PasswordForm.tsx
+++ frontend/src/components/PasswordForm.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import type { PasswordRequest } from "../services/api";
 
 interface props {
     onGenerate: (req: PasswordRequest) => void
     loading: boolean;
 }
+
+export function PasswordForm({ onGenerate, loading }: props) {
+    const [length, setLength] = useState(16);
+    const [uppercase, uppercase] = useState(true);
+    const [lowercase, lowercase] = useState(true);
+    const [numbers, numbers] = useState(true);
+    const [symbols, symbols] = useState(true);
+    const [excludeAmbiguous, serExcludeAbiguous] = useState(true);
+    const [quantity, setQuantity] = useState(1);
+
+    const noneSelected = !uppercase && !lowercase && !numbers && !symbols;
