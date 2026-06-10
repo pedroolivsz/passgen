24ca621bed193edad877809e714dd7a4da95d0b1
Pedro <joaopedropereira2209@gmail.com>
2026-06-10 12:08:09 -0300

    feat: Add function handleGenerate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,5 +6,17 @@ export default function App() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
-    
+    async function handleGenerate(request: PasswordRequest) {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const data = await generatePasswords(request);
+            setResult(data);
+        } catch (e: unknown) {
+            setError(e instanceof Error ? e.message : "Erro desconhecido");
+        } finally {
+            setLoading(false);
+        }
+    }
 }
